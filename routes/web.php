4dd22551644eb290978ff89c3f7e7837f149a271
Fabio Freitas <fabiofreitas.pj@gmail.com>
2022-07-18 11:08:05 -0300

    routes
    routes to controllers

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,38 @@
 
 Route::get('/', function () {
     $helloWorld = 'Hello World';
-    
-    //return view('welcome', ['x' => $helloWorld]);
+
     return view('welcome', compact('helloWorld'));
 });
+
+Route::get('/products', function () {
+    $products = \App\Product::All(); //select * from products
+
+    return $products;
+});
+
+Route::get('/users', function () {
+    
+    $user = new \App\User();
+    $user->name = 'Usuário Teste';
+    $user->email = 'email@teste.com';
+    $user->password = bcrypt('12345678');
+    //$user = \App\User::find(4);
+    //dd($user->store()->count());
+    //return \App\User::paginate(10);
+    return \App\User::all(); //all() = collection in json    
+
+    $loja = \App\Store::find(1);
+    return $loja->products->count();
+});
+
+Route::prefix('admin')->namespace('Admin')->group(function()
+{
+    Route::prefix('stores')->group(function() 
+    {
+        Route::get('stores', 'StoreController@index');
+        Route::get('create', 'StoreController@create');
+        Route::post('store', 'StoreController@store');
+    });    
+});
+ 
