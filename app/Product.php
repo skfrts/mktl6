cde9122efb7c0fb2cd8f7b334261602f1ff5c3a7
Fabio Freitas <fabiofreitas.pj@gmail.com>
2022-07-18 10:03:20 -0300

    Models and Relations

--- app/Product.php
+++ app/Product.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+
+    protected $fillable = ['name', 'description', 'body', 'price', 'slug'];
+
+    public function store()
+    {
+        return $this->belongsTo(Store::class);
+    }    
+
+    public function categories()
+    {
+        return $this->belongsToMany
+        .(Store::class);
+    }    
 }
